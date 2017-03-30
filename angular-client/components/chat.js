7f5c58c8d553fce7e1aa1969c9c3ebd94ec3b27f
E Shifrin <eshifrin@gmail.com>
2017-03-30 13:08:52 -0700

    integrated audio (w/o sockets)

--- angular-client/components/chat.js
+++ angular-client/components/chat.js
@@ -7,18 +7,15 @@ angular.module('app')
   this.translateTo = 'en';
   this.messages = [];
   this.chatting = true;
-<<<<<<< HEAD
 
-=======
   //chatting is a boolean passed to the interpreter directive & html
   //so we can hide features we don't want
->>>>>>> integrated audio
 
   isolineatorService.getChatLang((data) => {
     this.languages = data.data;
   });
 
-<<<<<<< HEAD
+
   this.sendMessage = () => {
     if (this.foreignText) {
       var message = { 
@@ -31,8 +28,6 @@ angular.module('app')
     }
   }
 
-=======
->>>>>>> integrated audio
   //waiting for the audio
   socket.on('transcription', (data, trans) => {
       this.foreignText = trans;
